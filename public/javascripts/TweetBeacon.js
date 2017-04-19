bd8b5a278fcdf6ce718afb89146e7a0a7882db35
p13kara33 <p13kara3@ionio.gr>
2017-04-19 18:52:36 +0300

    Update TweetBeacon.js

--- public/javascripts/TweetBeacon.js
+++ public/javascripts/TweetBeacon.js
@@ -95,7 +95,7 @@ TweetBeacon.prototype.addShockwave = function () {
 
   // Animates scale/size of shockwave
   TweenLite.to(circle.scale, time, {
-    x: 1.0, y: 1.0, z: 1.0,
+    x: 1.0, y: 10.0, z: 10.0,
     ease: Quart.easeOut,
     onComplete: function () {
       // remove when animation completes to keep number of object in scene to a minimum
